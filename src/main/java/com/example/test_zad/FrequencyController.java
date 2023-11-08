9231612721bda4281a5516a77986c705d794d613
Dmitriy <124405685+SerSul@users.noreply.github.com>
2023-11-08 18:51:11 +0300

    updated

--- src/main/java/com/example/test_zad/FrequencyController.java
+++ src/main/java/com/example/test_zad/FrequencyController.java
@@ -1,21 +1,27 @@
 package com.example.test_zad;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 import java.util.Map;
 import java.util.HashMap;
 
 
+
+
+
 @RestController
 public class FrequencyController {
 
-    @GetMapping("/calculateFrequency/{message}")
-    public Map<Character, Integer> calculateFrequency(@PathVariable String message) {
-        if (message == null) {
-            message = "";
+    @Operation(summary = "Подсчет частоты символов в строке", description = "Вычисляет частоту символов в данном сообщении.")
+    @PostMapping("/calculateFrequency")
+    public ResponseEntity<?> calculateFrequency(
+            @Parameter(description = "Сообщение для анализа") @RequestBody(required = false) String message) {
+        if (message == null || message.isEmpty()) {
+            return ResponseEntity.badRequest().body("Вы не ввели сообщение.");
         }
 
         char[] characters = message.toCharArray();
@@ -32,8 +38,6 @@ public class FrequencyController {
                 .sorted((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()))
                 .forEach(entry -> sortedFrequencyMap.put(entry.getKey(), entry.getValue()));
 
-        return sortedFrequencyMap;
+        return ResponseEntity.ok(sortedFrequencyMap);
     }
-
 }
-
--- src/test/java/com/example/test_zad/FrequencyControllerTest.java
+++ src/test/java/com/example/test_zad/FrequencyControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -21,7 +22,9 @@ class FrequencyControllerTest {
     void testCalculateFrequency() throws Exception {
         String message = "hello";
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/calculateFrequency/{message}", message))
+        mockMvc.perform(MockMvcRequestBuilders.post("/calculateFrequency")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("\"" + message + "\""))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.l").value(2))
@@ -30,16 +33,13 @@ class FrequencyControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.o").value(1));
     }
 
-
-
-
-
-
     @Test
     void testCalculateFrequencyWithSpecialCharacters() throws Exception {
         String message = "a!b@c#";
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/calculateFrequency/{message}", message))
+        mockMvc.perform(MockMvcRequestBuilders.post("/calculateFrequency")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("\"" + message + "\""))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.a").value(1))
@@ -50,5 +50,12 @@ class FrequencyControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.#").value(1));
     }
 
+    @Test
+    void testCalculateFrequencyWithMissingValue() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/calculateFrequency")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("")) // Пустое сообщение
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.content().string("Вы не ввели сообщение."));
+    }
 }
-
